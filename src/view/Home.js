409ba78427fbdd5312ef25e49b711fa6da3ad9e2
yukta-patel <yuktapatel.infynno@gmail.com>
2020-05-29 19:33:42 +0530

    committed changes

--- src/view/Home.js
+++ src/view/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
       //   newssingle: state.postsReducer.singleNews.news
       // }));
     
-        function showLastDiv(){
+      function showLastDiv(){
         document.getElementById('RightDiv').style.display = "block";
       }
 
@@ -48,11 +48,27 @@ const Home = () => {
         document.getElementById('RightDiv').style.display = "none";
       }
 
-      function HideDiv(){
-        document.getElementById('xyz').style.display = "block";
+      function HideCountryList(){
+        document.getElementById('CountryList').style.display="none";
+        document.getElementById('NewsTitle').style.display="block"
       }
 
-     return (
+      function HideNewsTitle(){
+        document.getElementById('NewsTitle').style.display="none";
+        document.getElementById('NewsDescription').style.display="block"
+      }
+
+      function backToCountrtyList(){
+        document.getElementById('CountryList').style.display="block";
+        document.getElementById('NewsTitle').style.display="none";
+      }
+
+      function backToTitles(){
+        document.getElementById('NewsTitle').style.display="block";
+        document.getElementById('NewsDescription').style.display="none";
+      }
+
+  return (
   <div>
 
     <div>
@@ -65,87 +81,6 @@ const Home = () => {
          
           <div className="col-sm-2">
             <CountryList />
-            {/* <div className="left-div">
-              <div className="row mt-1">
-                <div className="col-4"><img className="flag" src={india} alt="indian-flag" onClick={() => {dispatch(fetchNews("in"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("in"));HideLastDiv()}}>INDIA</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={australia} alt="australia-flag" onClick={() => {dispatch(fetchNews("au"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("au"));HideLastDiv()}}>AUSTRALIA</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={austris} alt="austria-flag" onClick={() => {dispatch(fetchNews("at"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("at"));HideLastDiv()}}>AUSTRIA</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={belgium} alt="belgium-flag" onClick={() => {dispatch(fetchNews("be"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("be"));HideLastDiv()}}>BELGIUM</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={brazil} alt="brazil-flag" onClick={() => {dispatch(fetchNews("br"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("br"));HideLastDiv()}}>BRAZIL</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={canada} alt="canada-flag" onClick={() => {dispatch(fetchNews("ca"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ca"));HideLastDiv()}}>CANADA</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={china} alt="china-flag" onClick={() => {dispatch(fetchNews("cn"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("cn"));HideLastDiv()}}>CHINA</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={egypt} alt="egypt-flag" onClick={() => {dispatch(fetchNews("eg"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("eg"));HideLastDiv()}}>EGYPT</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={france} alt="france-flag" onClick={() => {dispatch(fetchNews("fr"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("fr"));HideLastDiv()}}>FRANCE</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={germany} alt="germany-flag" onClick={() => {dispatch(fetchNews("de"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("de"));HideLastDiv()}}>GERMANY</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={greece} alt="greece-flag" onClick={() => {dispatch(fetchNews("gr"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("gr"));HideLastDiv()}}>GREECE</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={hungary} alt="hungary-flag" onClick={() => {dispatch(fetchNews("hu"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("hu"));HideLastDiv()}}>HUNGARY</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={ireland} alt="ireland-flag" onClick={() => {dispatch(fetchNews("ie"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ie"));HideLastDiv()}}>IRELAND</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={japan} alt="japan-flag" onClick={() => {dispatch(fetchNews("jp"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("jp"));HideLastDiv()}}>JAPAN</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={mexico} alt="mexico-flag" onClick={() => {dispatch(fetchNews("mx"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("mx"));HideLastDiv()}}>MEXICO</p></div>
-              </div>
-
-              <div className="row">
-                <div className="col-4"><img className="flag" src={ukraine} alt="ukraine-flag" onClick={() => {dispatch(fetchNews("ua"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ua"));HideLastDiv()}}>UKRAINE</p></div>
-              </div>
-            </div> */}
           </div> 
 
           {/* <div className="col-sm-4">
@@ -201,136 +136,141 @@ const Home = () => {
             </div>
           </div>
 
-          {/* <div className="col-sm-6">
-            <div className="right-div">  
-            {singleloaging ? (
-                <div>Loding...</div>
-                ) : (
-                <div>
-                  {singlenews !== null &&
-                    singlenews.map(newsingle => (
-                      <div key={newsingle.id}>
-                        <div className="news-title-div">
-                          <div className="news-title">{newsingle.title}</div>
-                        </div>
-                      </div>
-                    ))}
-
-                </div>
-                )}
-            </div>
-          </div> */}
-
 
       </div>
     </div>
-      
-    <div className="dropdown-div">
+
+    <div className="mobile-dropdown-div">
+
+          <div id="CountryList">
       
               <div className="row mt-1">
                 <div className="col-4"><img className="flag" src={india} alt="indian-flag" onClick={() => {dispatch(fetchNews("in"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("in"));HideDiv()}}>INDIA</p></div>
-
-                    <div className="xyz" id="xyz"> 
-                    {loading ? (
-                    <div>Loding...</div>
-                    ) : (
-                    <div>
-                      {news !== null &&
-                        news.map(news => (
-                          <div>
-                            <div className="news-title-div">
-                              <div className="news-title">{news.title}</div>
-                              <div className="news-source">-{news.source.name}</div>
-                              <div className="read-more"><p onClick={() => {setsinglenews(news);showLastDiv()}}>Read More</p></div>
-                            </div>
-
-                            <img className="right-img" src={singleNews.urlToImage} />
-                          </div>
-                        ))}
-
-                    </div>
-                  )}
-                  
-                  </div>
-                </div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("in"));HideCountryList()}}>INDIA</p></div>
+              </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={australia} alt="australia-flag" onClick={() => {dispatch(fetchNews("au"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("au"));HideLastDiv()}}>AUSTRALIA</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("au"));HideCountryList()}}>AUSTRALIA</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={austris} alt="austria-flag" onClick={() => {dispatch(fetchNews("at"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("at"));HideLastDiv()}}>AUSTRIA</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("at"));HideCountryList()}}>AUSTRIA</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={belgium} alt="belgium-flag" onClick={() => {dispatch(fetchNews("be"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("be"));HideLastDiv()}}>BELGIUM</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("be"));HideCountryList()}}>BELGIUM</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={brazil} alt="brazil-flag" onClick={() => {dispatch(fetchNews("br"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("br"));HideLastDiv()}}>BRAZIL</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("br"));HideCountryList()}}>BRAZIL</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={canada} alt="canada-flag" onClick={() => {dispatch(fetchNews("ca"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ca"));HideLastDiv()}}>CANADA</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ca"));HideCountryList()}}>CANADA</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={china} alt="china-flag" onClick={() => {dispatch(fetchNews("cn"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("cn"));HideLastDiv()}}>CHINA</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("cn"));HideCountryList()}}>CHINA</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={egypt} alt="egypt-flag" onClick={() => {dispatch(fetchNews("eg"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("eg"));HideLastDiv()}}>EGYPT</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("eg"));HideCountryList()}}>EGYPT</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={france} alt="france-flag" onClick={() => {dispatch(fetchNews("fr"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("fr"));HideLastDiv()}}>FRANCE</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("fr"));HideCountryList()}}>FRANCE</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={germany} alt="germany-flag" onClick={() => {dispatch(fetchNews("de"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("de"));HideLastDiv()}}>GERMANY</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("de"));HideCountryList()}}>GERMANY</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={greece} alt="greece-flag" onClick={() => {dispatch(fetchNews("gr"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("gr"));HideLastDiv()}}>GREECE</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("gr"));HideCountryList()}}>GREECE</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={hungary} alt="hungary-flag" onClick={() => {dispatch(fetchNews("hu"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("hu"));HideLastDiv()}}>HUNGARY</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("hu"));HideCountryList()}}>HUNGARY</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={ireland} alt="ireland-flag" onClick={() => {dispatch(fetchNews("ie"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ie"));HideLastDiv()}}>IRELAND</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ie"));HideCountryList()}}>IRELAND</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={japan} alt="japan-flag" onClick={() => {dispatch(fetchNews("jp"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("jp"));HideLastDiv()}}>JAPAN</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("jp"));HideCountryList()}}>JAPAN</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={mexico} alt="mexico-flag" onClick={() => {dispatch(fetchNews("mx"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("mx"));HideLastDiv()}}>MEXICO</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("mx"));HideCountryList()}}>MEXICO</p></div>
               </div>
 
               <div className="row">
                 <div className="col-4"><img className="flag" src={ukraine} alt="ukraine-flag" onClick={() => {dispatch(fetchNews("ua"))}}/></div>
-                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ua"));HideLastDiv()}}>UKRAINE</p></div>
+                <div className="col-6 country-name"><p onClick={() => {dispatch(fetchNews("ua"));HideCountryList()}}>UKRAINE</p></div>
               </div>
+
+        </div>
+
+        {/* <select>
+          <option onChange={() => {dispatch(fetchNews("in"))}}>India</option>
+          <option >Austria</option>
+          <option >Belgium</option>
+          <option >Canada</option>
+          <option >United States</option>
+          <option >India</option>
+          <option >China</option>
+          <option >Columbia</option>
+          <option >Cuba</option> 
+        </select> */}
+
       
-    </div> 
+
+        <div className="mobile-title-div" id="NewsTitle"> 
+          <button className="back-btn" onClick={backToCountrtyList}>Back</button>
+              {loading ? (
+                <div>Loding...</div>
+                ) : (
+                <div>
+                  {news !== null &&
+                    news.map(news => (
+                        <div className="news-title-div">
+                          <div className="news-title">{news.title}</div>
+                          <div className="news-source">-{news.source.name}</div>
+                          <div className="read-more"><p onClick={() => {setsinglenews(news);HideNewsTitle()}}>Read More</p></div>
+                        </div>
+                    ))}
+
+                </div>
+              )}
+        </div>
+
+        <div className="mobile-description-div" id="NewsDescription">
+          <button className="back-btn" onClick={backToTitles}>Back</button>
+          <div className="right-title">{singleNews.title}</div>
+          <p className="published-at">Publishes at : {singleNews.publishedAt}</p>
+          <img className="right-img" src={singleNews.urlToImage} />
+          <div className="right-description">{singleNews.description}</div>
+          <div className="right-description">{singleNews.content}</div>
+          <div><p className="mt-3 more-information">For more information <a href={singleNews.url}>click here</a></p></div>
+        </div>
+
+      </div> 
+    
   </div>
 
        
